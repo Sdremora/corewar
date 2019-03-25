5871361c4749af4e2c126cce8a1e95520942187f
Hakon Harvey <hharvey@e3r3p6.21-school.ru>
2019-03-25 17:02:50 +0300

    clean

--- src/corewar/fight.c
+++ src/corewar/fight.c
@@ -1,28 +1,5 @@
 #include "corewar.h"
 
-
-int			check_arg_bites(int op_id, int arg)
-{
-	if (arg == 1)
-	{
-		if (op_id & 1)
-			return (1);
-	}
-	if (arg == 2)
-	{
-		if ((op_id >> 1) & 1)
-			return (1);
-	}
-	if (arg == 3)
-	{
-		if (op_id > 3)
-			return (1);
-	}
-	if (!arg && !op_id)
-		return (1);
-	return (0);
-}
-
 int			step_bites(int arg, int dir_size) //счетчик отступов для аргументов
 {
 	if (arg == T_REG)
@@ -36,36 +13,6 @@ int			step_bites(int arg, int dir_size) //счетчик отступов для
 	return (-42);
 }
 
-int			step_args(int t_dir_size_eq_2, unsigned char arg)
-{
-	int res;
-
-	res = 0;
-	res += step_bites(t_dir_size_eq_2, arg >> 6);
-	res += step_bites(t_dir_size_eq_2, arg >> 4 & 3);
-	res += step_bites(t_dir_size_eq_2, arg >> 2 & 3);
-	return (res);
-}
-/*
-int			check_code_args(t_carriage *carg, t_arena *arena)
-{
-	unsigned char	oper;
-	unsigned char	arg;
-	int				res;
-
-	res = 1;
-	oper = arena->map[carg->mem_pos] - 1;
-	arg = arena->map[(carg->mem_pos + 1) % MEM_SIZE];
-	carg->args[0] = arg >> 6;
-	carg->args[1] = arg >> 4 & 3;
-	carg->args[2] = arg >> 2 & 3;
-	res *= check_arg_bites(g_op_tab[oper].args[0], carg->args[0]);
-	res *= check_arg_bites(g_op_tab[oper].args[1], carg->args[1]);
-	res *= check_arg_bites(g_op_tab[oper].args[2], carg->args[2]);
-	return (res);
-}
-*/
-
 static void	operations_sort(t_carriage *carg, t_arena *arena)
 {
 	unsigned char	oper;
@@ -73,16 +20,8 @@ static void	operations_sort(t_carriage *carg, t_arena *arena)
 	oper = arena->map[carg->mem_pos] - 1;
 	if (oper >= LIVE && oper <= AFF)
 	{
-//		if (check_code_args(oper, arena->map[(carg->mem_pos + 1) % MEM_SIZE]))
-//		{
 			carg->pause_count = g_op_tab[oper].cost;
 			carg->op_id = oper;
-//		}
-//		else
-//		{
-//			carg->mem_pos = (carg->mem_pos + 2 + step_args(g_op_tab[oper].dir_size, arena->map[(carg->mem_pos + 1) % MEM_SIZE])) % MEM_SIZE;
-//		}
-//		move_carriage(carg, arena);
 	}
 	else
 		op_invalid(carg, arena);
@@ -117,7 +56,6 @@ int			check_code_args(int args[4], unsigned char op_id)
 	int	res;
 	int i;
 
-
 	res = 1;
 	i = 0;
 	while (i < 5)
@@ -140,8 +78,6 @@ void		move_carriage(t_carriage *carg, int dir_size)
 	move += step_bites(carg->args[0], dir_size) + step_bites(carg->args[1], dir_size)
 			+ step_bites(carg->args[2], dir_size) + step_bites(carg->args[3], dir_size);
 	carg->mem_pos = (carg->mem_pos + move) % MEM_SIZE;
-	
-
 }
 
 static void	play_round(t_arena *arena)
@@ -161,6 +97,7 @@ static void	play_round(t_arena *arena)
 			if (check_code_args(carg->args, arena->map[carg->mem_pos] - 1))
 				g_op_tab[carg->op_id].op_handler(carg, arena);
 			move_carriage(carg, g_op_tab[arena->map[carg->mem_pos] - 1].dir_size);
+			carg->op_id = -1;
 		}
 		else
 			operations_sort(carg, arena);
