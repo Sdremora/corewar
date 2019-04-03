4828d3f5ba9ad145bda6e196771ae10b7424e3e0
Marie Narbert <mnarbert@e4r7p2.21-school.ru>
2019-04-03 09:38:19 +0300

    push before logout

--- src/asm/algorithm.c
+++ src/asm/algorithm.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   algorithm.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkihn <kkihn@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mnarbert <mnarbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/02 11:47:42 by mnarbert          #+#    #+#             */
-/*   Updated: 2019/04/02 19:52:27 by kkihn            ###   ########.fr       */
+/*   Updated: 2019/04/03 09:27:03 by mnarbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int			find_right_label(int i, char *label)
 			}
 			else
 			{
-				while (j > i)
+				while (j >= i)
 				{
 					counter += g_struct[j].byte;
 					j--;
@@ -79,8 +79,8 @@ void		handle_arg(char *arg, int i, int *k, char *pre_matrix, int shift)
 			counter = ft_atoi_long(&arg[1]);
 		printf("counter: %lld\n", counter);
 		if (counter < 0)
-			counter = (counter * (-1)) ^ 4294967295 + 1;
-		temp = ft_itoa_base(counter, 16, 1);
+			counter = (counter * (-1)) ^ 4294967295;
+		temp = ft_itoa_base(counter + 1, 16, 1);
 		counter = (g_op_tab[g_struct[i].id_in_tab].t_dir_size_eq_4 ==
 				0) ? 8 : 4;
 		*k += counter - 1;
@@ -143,10 +143,9 @@ void    realise_algorithm(void)
 			j = 0;
 			pre_matrix = ft_strnew(g_struct[i].byte * 2);
 			ft_memset(pre_matrix, '0', g_struct[i].byte * 2);
-			printf("pre-matrix: %s\n", pre_matrix);
 			pre_matrix[1] = (g_op_tab[g_struct[i].id_in_tab].id > 9) ?
 				g_op_tab[g_struct[i].id_in_tab].id % 10 + 'A' :
-					g_op_tab[g_struct[i].id_in_tab].id;
+					g_op_tab[g_struct[i].id_in_tab].id + '0';
 			k = (g_op_tab[g_struct[i].id_in_tab].kod_tipov_argumenta == 1) ? 4 : 2;
 			while(++j <= g_op_tab[g_struct[i].id_in_tab].var_count)
 			{
